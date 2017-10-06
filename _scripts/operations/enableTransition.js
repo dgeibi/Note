ba35802333e32d917c315529535cfa25a10e9240
dgeibi <png.inside@gmail.com>
2017-10-06 11:42:20 +0800

    disable transition preload

---
+++ _scripts/operations/enableTransition.js
@@ -0,0 +1,8 @@
+const removeClass = (className) => {
+  const node = document.querySelector(`.${className}`)
+  if (node) {
+    node.classList.remove(className)
+  }
+}
+
+removeClass('_disable-transition')
